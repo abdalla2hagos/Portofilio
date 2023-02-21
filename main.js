1e559bc2174248336fe4436b5f1267dc57b13114
abdalla <abdalla.hagos@gmail.com>
2023-02-20 19:17:22 -0500

    First Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '082a4fe00emshf046964dbf63da6p1469cbjsn3368b0e1bdda',
+// 		'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://spotify23.p.rapidapi.com/search/?q=%3CREQUIRED%3E&type=multi&offset=0&limit=10&numberOfTopResults=5', options)
+// 	.then(response => response.json())
+// 	.then(response => {
+        
+//             response.albums.items.filter(response=>{
+//                 console.log(response.data.date.year === 2020)
+//             })
+//     })
+// 	.catch(err => console.error(err));
